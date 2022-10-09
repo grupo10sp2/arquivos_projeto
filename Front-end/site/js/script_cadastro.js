c48ffcacaa09d105056194584a5518266f42adaf
EduardoDamSilva <110927182+EduardoDamSilva@users.noreply.github.com>
2022-10-09 12:19:09 -0300

    Add files via upload

--- Front-end/site/js/script_cadastro.js
+++ Front-end/site/js/script_cadastro.js
@@ -41,3 +41,38 @@ function cadastrar(){
         location = "login.html";
     }
 }
+
+function cadastrarFuncionario(){
+    var nome = input_nome.value;
+    var email = input_email.value;
+    var senha = input_senha.value;
+    var confirmacao_senha = input_confirmacao_senha.value;
+
+    var cadastro_valido = true;
+
+    if (nome == "" || nome[0] == " " || nome.length < 3){
+        input_nome.style.borderColor = "#FF0000";
+        cadastro_valido = false;
+    }
+    if (email == "" ||
+        email.indexOf(" ") > -1 ||
+        email.indexOf("@") < 3 ||
+        (email.lastIndexOf(".") - email.indexOf("@")) < 4 ||
+        (email.length - email.lastIndexOf(".")) < 3){
+        input_email.style.borderColor = "#FF0000";
+        cadastro_valido = false;
+    }
+    if (senha == "" || senha[0] == " " || senha.length < 5){
+        input_senha.style.borderColor = "#FF0000";
+        cadastro_valido = false;
+    }
+    if (confirmacao_senha == "" || confirmacao_senha[0] == " " || confirmacao_senha != senha){
+        input_confirmacao_senha.style.borderColor = "#FF0000";
+        cadastro_valido = false;
+    }
+
+    if (cadastro_valido){
+        alert("Cadastro realizado com sucesso!")
+        location = "login.html";
+    }
+}
